c0af27b98a636427148143965800bdf71f647eb1
biggani710 <nguyenvanbebu2k5@gmail.com>
2024-05-31 14:57:07 +0700

    Giang

--- Assets/Code/PlayerColltroler.cs
+++ Assets/Code/PlayerColltroler.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
@@ -9,24 +9,29 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10f;
     public float jumpForce = 25f;
     public float dashForce = 20f;
-    public float DashTime = 0.3f;
+    public float dashTime = 0.3f;
     private Rigidbody2D rb;
     public Transform groundCheckObject;
     private SpriteRenderer spriteRenderer;
     private bool isGrounded;
     private bool isDashing = false;
     private bool hasJump = false;
-    public GameObject DashEffectObject;
-    private Vector2 moveInput;
+    public GameObject dashEffectObject;
+    private Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = rb.GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
+
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheckObject.position, 0.2f, LayerMask.GetMask("Ground"));
         float moveInput = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("isRunning", Mathf.Abs(moveInput));
+
         if (!isDashing)
         {
             rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
@@ -57,15 +62,29 @@ public class PlayerController : MonoBehaviour
         float dashDirection = spriteRenderer.flipX ? -1f : 1f;
         rb.velocity = new Vector2(dashForce * dashDirection, rb.velocity.y);
         isDashing = true;
-        DashEffectObject.SetActive(true);
+        dashEffectObject.SetActive(true);
         StartCoroutine(StopDash());
     }
 
     IEnumerator StopDash()
     {
-        yield return new WaitForSeconds(DashTime);
+        yield return new WaitForSeconds(dashTime);
         rb.velocity = new Vector2(0f, rb.velocity.y);
         isDashing = false;
-        DashEffectObject.SetActive(false);
+        dashEffectObject.SetActive(false);
+    }
+
+    // Hàm này được gọi từ animation event khi kết thúc animation nhảy
+    public void EndJump()
+    {
+        animator.SetBool("isJump", false);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            animator.SetBool("isJump", false);
+        }
     }
 }
