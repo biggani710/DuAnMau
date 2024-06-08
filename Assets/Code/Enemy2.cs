007266b3aaf806adbc6c3c96d1f88147c95d930f
biggani710 <nguyenvanbebu2k5@gmail.com>
2024-06-08 14:19:54 +0700

    Giang

---
+++ Assets/Code/Enemy2.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        GameObject playerobject = GameObject.FindGameObjectWithTag("Player");
+        if(playerobject == null)
+        {
+            playerobject = FindObjectOfType<GameObject>();
+        }
+        if(playerobject != null)
+        {
+            playerTransform =playerobject.transform;
+        }
+        else
+        {
+            Debug.Log("No player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform != null)
+        {
+            Vector3 direction = (playerTransform.position - transform.position).normalized;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
