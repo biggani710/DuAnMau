714bf9e2d0ca8f068b3a10567c0d1bb22b95709a
biggani710 <nguyenvanbebu2k5@gmail.com>
2024-06-18 03:23:17 +0700

    Giang

---
+++ Assets/Code/Boss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        GameObject playerobject = GameObject.FindGameObjectWithTag("Player");
+        if (playerobject == null)
+        {
+            playerobject = FindObjectOfType<GameObject>();
+        }
+        if (playerobject != null)
+        {
+            playerTransform = playerobject.transform;
+        }
+        else
+        {
+            Debug.Log("No player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            Vector3 direction = (playerTransform.position - transform.position).normalized;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
+}
